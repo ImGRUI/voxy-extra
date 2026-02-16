0ebdea205b56aa6f4861f34764cd0134465f643b
ImGRUI <91282421+ImGRUI@users.noreply.github.com>
2026-02-16 19:29:41 +0500

    Further fix

--- src/main/java/me/imgrui/mixin/minecraft/MinecraftMixin.java
+++ src/main/java/me/imgrui/mixin/minecraft/MinecraftMixin.java
@@ -1,6 +1,7 @@
 package me.imgrui.mixin.minecraft;
 
 import me.cortex.voxy.client.config.VoxyConfig;
+import me.cortex.voxy.client.core.util.IrisUtil;
 import me.imgrui.VoxyExtra;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
@@ -15,6 +16,7 @@ public class MinecraftMixin {
     public void voxyExtra$handleBlacklist(Screen screen, boolean bl, boolean bl2, CallbackInfo ci) {
         if (VoxyExtra.isInBlacklist && !VoxyConfig.CONFIG.enabled) {
             VoxyConfig.CONFIG.enabled = true;
+            IrisUtil.reload();
         }
         VoxyExtra.isInBlacklist = false;
     }
